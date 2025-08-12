f99ba188400932cb8c1ad8708902c6078b52c57a
Aleksandra Kuzeleva <a.brods@gmail.com>
2025-08-12 19:59:44 +0200

    commit

--- BeautySalonBooking/obj/Debug/net8.0/BeautySalonBooking.AssemblyInfo.cs
+++ BeautySalonBooking/obj/Debug/net8.0/BeautySalonBooking.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautySalonBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69d2ac536809c097b991b977ab421c848e77db81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27ccb0ffe52e7cabf6575d45020a6b8278076a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautySalonBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautySalonBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
